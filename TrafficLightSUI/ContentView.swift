6bd03154710fb20d948d331feabfb815fc778e74
dutysniper <natarov.constantin@yandex.ru>
2023-06-08 19:57:44 +0300

    //

--- TrafficLightSUI/ContentView.swift
+++ TrafficLightSUI/ContentView.swift
@@ -8,18 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let lightOff = 0.3
+    private let lightOn = 1.0
+    
+    @State private var changeColor: ChangeColor = .red
+    @State private var redLightOpacity = 0.3
+    @State private var yellowLightOpacity = 0.3
+    @State private var greenLightOpacity = 0.3
+    @State private var buttonName = "START"
+    
     var body: some View {
         VStack {
-            LightView(color: .red)
-            LightView(color: .yellow)
-            LightView(color: .green)
+            VStack(spacing: 40) {
+                LightView(color: .red)
+                    .opacity(redLightOpacity)
+                LightView(color: .yellow)
+                    .opacity(yellowLightOpacity)
+                LightView(color: .green)
+                    .opacity(greenLightOpacity)
+            }
+            .animation(.easeInOut(duration: 0.8), value: changeColor)
+            .padding()
+            Spacer()
+            Button(action: changeLight) {
+                Text(buttonName)
+                    .foregroundColor(.white)
+                    .padding(15)
+                    .background(Color.blue)
+                    .cornerRadius(20)
+            }
         }
-        .padding()
+    }
+    
+    private func changeLight() {
+        if buttonName == "START" {
+            buttonName = "NEXT"
+        }
+        switch changeColor {
+        case .red:
+            greenLightOpacity = lightOff
+            redLightOpacity = lightOn
+            changeColor = .yellow
+        case .yellow:
+            redLightOpacity = lightOff
+            yellowLightOpacity = lightOn
+            changeColor = .green
+        case .green:
+            yellowLightOpacity = lightOff
+            greenLightOpacity = lightOn
+            changeColor = .red
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    enum ChangeColor {
+        case red
+        case yellow
+        case green
     }
 }
--- TrafficLightSUI/LightView.swift
+++ TrafficLightSUI/LightView.swift
@@ -11,26 +11,35 @@ struct LightView: View {
     let color: Color
     
     var body: some View {
-        GeometryReader { geometry in
+        ZStack {
             Circle()
-                .frame(
-                    width:
-                        geometry.size.width * 0.35,
-                    height:
-                        geometry.size.height * 0.35
-                )
+                .frame(width: 150, height: 150)
                 .foregroundColor(color)
-                .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
-                .position(
-                    x: geometry.size.width / 2,
-                    y: geometry.size.height / 2
+                .overlay(Circle().stroke(lineWidth: 2))
+            
+            Circle()
+                .frame(width: 150, height: 150)
+                .foregroundColor(color.opacity(0))
+                .overlay(
+                    Circle()
+                        .stroke(lineWidth: 10)
+                        .blur(radius: 10)
+                        .offset(x: 0, y: 0)
+                        .mask(
+                            Circle()
+                                .frame(width: 150, height: 150)
+                                .foregroundColor(.white)
+                                .blur(radius: 10)
+                        )
                 )
+                .shadow(color: color.opacity(0.7), radius: 10)
         }
     }
 }
 
+
 struct LightView_Previews: PreviewProvider {
     static var previews: some View {
-        LightView(color: .brown)
+        LightView(color: .red)
     }
 }
